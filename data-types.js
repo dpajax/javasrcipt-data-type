cb9d1464722266d92bac55a5b15af81dd735bb9e
dpajax <oladapoajanaku@gmail.com>
2022-03-09 17:01:43 +0100

    data type

---
+++ data-types.js
@@ -0,0 +1,40 @@
+// String Method
+
+let brand ="Gucci"
+console.log(brand.toUpperCase())
+console.log(brand.toLowerCase())
+console.log(brand.repeat(10))
+console.log(brand.replace('cci', 'tter'))
+
+
+let x = '   Heyy     '
+console.log(x.trim())
+console.log(brand.length)
+
+console.log(brand[1])
+let phase = 'Agege Bread'
+console.log(phase.split(' '))
+
+
+let sliceBread = phase.slice(1 ,6 )
+console.log(sliceBread)
+
+//Number
+let num = 23
+console.log(num + 7)
+
+//covert numbers to string
+console.log(num.toString())
+
+//Add fixed decimals
+console.log(num.toFixed(3))
+console.log(num.toExponential(4))
+
+//.Infinity
+console.log(Number.isFinite(68764325678).toString().length)
+let d = "yam"
+console.log(Number.isFinite(d))
+console.log(Number.isNaN(d))
+console.log(Number.isFinite(Infinity).toString().length)
+
+//console.log(Math)
